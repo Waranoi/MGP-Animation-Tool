212bca0fc9cf3ca8f0ed4d24b51c6d5f5fe7b5cf
Elias Waranoi <e.waranoi@gmail.com>
2019-01-04 12:17:41 +0100

    Fixed missing files in last commit

---
+++ source/core/Window/glad/src/glad.d
@@ -0,0 +1,7 @@
+source/core/Window/glad/src/glad.o: source/core/Window/glad/src/glad.c \
+ source/core/Window/glad/include/glad/glad.h \
+ source/core/Window/glad/include/KHR/khrplatform.h
+
+source/core/Window/glad/include/glad/glad.h:
+
+source/core/Window/glad/include/KHR/khrplatform.h:
