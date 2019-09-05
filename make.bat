910fed38ea529fecfe0bfba6de85477c34112872
Elias Waranoi <e.waranoi@gmail.com>
2019-09-05 12:53:12 +0200

    Correct syntax for comments in batch scripts

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
-# Run make command by running the make.bat file or running ".\make.bat" in a console
-# Running the make command through the console lets you add your own parameters. Example ".\make.bat clean"
+rem Run make command by running the make.bat file or running ".\make.bat" in a console
+rem Running the make command through the console lets you add your own parameters. Example ".\make.bat clean"
 call source\core\Window\MinGW\set_distro_paths.bat
 call source\core\Window\MinGW\bin\make.exe %*
 pause
