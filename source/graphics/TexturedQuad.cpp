6a53fde00644492890a8a59853634091a530d66f
Elias Waranoi <e.waranoi@gmail.com>
2019-09-17 12:51:36 +0200

    TexQuadObj replacement (1/3) Replaced TexQuadObj with TextureQuadObject shared pointer in TexturedQuad files

--- source/graphics/TexturedQuad.cpp
+++ source/graphics/TexturedQuad.cpp
@@ -5,11 +5,6 @@
 
 static const int QUAD_ELEMENT_COUNT = 6;
 
-// Invalid default value not set
-static bool invalidSet = false;
-// Invalid default value
-static GLuint invalid;
-
 TextureQuadObject::TextureQuadObject()
 {
 
@@ -22,23 +17,16 @@ TextureQuadObject::~TextureQuadObject()
     glDeleteBuffers(1, &tex);
 }
 
-TexturedQuad::TexQuadObj::TexQuadObj()
+std::shared_ptr<TextureQuadObject> TexturedQuad::CreateQuad(std::string texSource)
 {
-    // If invalid default value has not been set
-    if (!invalidSet)
+    int comp, width, height;
+    unsigned char *texData = stbi_load(texSource.c_str(), &width, &height, &comp, STBI_rgb_alpha);
+    if (!texData)
     {
-        // Generate buffer value and set it as invalid default value
-        invalidSet = true;
-        glGenBuffers(1, &invalid);
+        printf("WARNING: Failed to create quad from source %s\n\n", texSource.c_str());
+        return std::shared_ptr<TextureQuadObject>();
     }
-    
-    // Set default value to be the same as the invalid default value
-    vbo = ebo = tex = invalid;
-}
 
-TexturedQuad::TexQuadObj TexturedQuad::CreateQuad(std::string texSource)
-{
-    TexQuadObj newTexQuad;
     float quadVB[] = 
     {
 		-1,	-1,	0,
@@ -55,45 +43,50 @@ TexturedQuad::TexQuadObj TexturedQuad::CreateQuad(std::string texSource)
     };
     int quadEB[] = { 0, 1, 2, 0, 2, 3 };
 
-	glGenBuffers(1, &newTexQuad.vbo);   
-	glBindBuffer(GL_ARRAY_BUFFER, newTexQuad.vbo);
+    TextureQuadObject *newTexQuad = new TextureQuadObject();
+
+	glGenBuffers(1, &newTexQuad->vbo);   
+	glBindBuffer(GL_ARRAY_BUFFER, newTexQuad->vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVB), quadVB, GL_STATIC_DRAW);
 
-    glGenBuffers(1, &newTexQuad.ebo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, newTexQuad.ebo);
+    glGenBuffers(1, &newTexQuad->ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, newTexQuad->ebo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadEB), quadEB, GL_STATIC_DRAW);
-
-    int comp, width, height;
-    unsigned char *texData = stbi_load(texSource.c_str(), &width, &height, &comp, STBI_rgb_alpha);
     
     glActiveTexture(GL_TEXTURE0);
-    glGenTextures(1, &newTexQuad.tex); 
-    glBindTexture(GL_TEXTURE_2D, newTexQuad.tex);
+    glGenTextures(1, &newTexQuad->tex); 
+    glBindTexture(GL_TEXTURE_2D, newTexQuad->tex);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, texData);
 
-    return newTexQuad;
+    stbi_image_free(texData);
+
+    return std::make_shared<TextureQuadObject>(newTexQuad);
 }
 
-TexturedQuad::TexQuadObj TexturedQuad::CreateQuad(std::string texSource, Vector2i texOrig, Vector2i texSize)
+std::shared_ptr<TextureQuadObject> TexturedQuad::CreateQuad(std::string texSource, Vector2i texOrig, Vector2i texSize)
 {
-    TexQuadObj newTexQuad;
-    Vector2i size;
-    Vector2f sizeInv, texOrigNorm, texSizeNorm, texBorderNorm;
     int comp;
-
+    Vector2i size;
     unsigned char *texData = stbi_load(texSource.c_str(), &size.x, &size.y, &comp, STBI_rgb_alpha);
-    sizeInv = Vector2f(1.0f/size.x, 1.0f/size.y);
-    texOrigNorm = sizeInv * texOrig;
-    texSizeNorm = sizeInv * texSize;
-    texBorderNorm = texOrigNorm + texSizeNorm;
+    if (!texData)
+    {
+        printf("WARNING: Failed to create quad from source %s\n\n", texSource.c_str());
+        return std::shared_ptr<TextureQuadObject>();
+    }
+
+    Vector2f sizeInv, texOrigNorm, texSizeNorm, texBorderNorm;
+    Vector2f sizeInv = Vector2f(1.0f/size.x, 1.0f/size.y);
+    Vector2f texOrigNorm = sizeInv * texOrig;
+    Vector2f texSizeNorm = sizeInv * texSize;
+    Vector2f texBorderNorm = texOrigNorm + texSizeNorm;
 
     if (texOrigNorm.x < 0 || texOrigNorm.y < 0 || texOrigNorm.x > size.x || texOrigNorm.y > size.y ||
         texBorderNorm.x < 0 || texBorderNorm.y < 0 || texBorderNorm.x > size.x || texBorderNorm.y > size.y)
     {
-        printf("WARNING: Texture coordinates extend beyond source image\n");
-        return newTexQuad;
+        printf("WARNING: Texture coordinates extend beyond source image\n\n");
+        return std::shared_ptr<TextureQuadObject>();
     }
 
     float quadVB[] = 
@@ -112,33 +105,34 @@ TexturedQuad::TexQuadObj TexturedQuad::CreateQuad(std::string texSource, Vector2
     };
     int quadEB[] = { 0, 1, 2, 0, 2, 3 };
 
-	glGenBuffers(1, &newTexQuad.vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, newTexQuad.vbo);
+    TextureQuadObject *newTexQuad = new TextureQuadObject();
+
+	glGenBuffers(1, &newTexQuad->vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, newTexQuad->vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVB), quadVB, GL_STATIC_DRAW);
 
-    glGenBuffers(1, &newTexQuad.ebo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, newTexQuad.ebo);
+    glGenBuffers(1, &newTexQuad->ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, newTexQuad->ebo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadEB), quadEB, GL_STATIC_DRAW);
     
     glActiveTexture(GL_TEXTURE0);
-    glGenTextures(1, &newTexQuad.tex); 
-    glBindTexture(GL_TEXTURE_2D, newTexQuad.tex);
+    glGenTextures(1, &newTexQuad->tex); 
+    glBindTexture(GL_TEXTURE_2D, newTexQuad->tex);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size.x, size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, texData);
 
-    return newTexQuad;
+    stbi_image_free(texData);
+
+    return std::make_shared<TextureQuadObject>(newTexQuad);
 }
 
-TexturedQuad::TexQuadObj TexturedQuad::UpdateQuad(TexQuadObj quad)
+void TexturedQuad::UpdateQuad(std::shared_ptr<TextureQuadObject> quad)
 {
-    return quad;
-}
+    if (!quad)
+        return;
 
-bool TexturedQuad::IsValidTexQuad(TexQuadObj quad)
-{
-    // If quad values are the same as the invalid default value then the quad is invalid
-    return quad.vbo != invalid && quad.ebo != invalid && quad.tex != invalid;
+    // To be done, maybe
 }
 
 void TexturedQuad::InitQuadDrawing()
@@ -153,13 +147,16 @@ void TexturedQuad::InitQuadDrawing()
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (GLvoid*)(sizeof(float) * 3));
 }
 
-void TexturedQuad::BindQuad(TexQuadObj quad)
+void TexturedQuad::BindQuad(std::shared_ptr<TextureQuadObject> quad)
 {
-    glBindBuffer(GL_ARRAY_BUFFER, quad.vbo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quad.ebo);
+    if (!quad)
+        return;
+
+    glBindBuffer(GL_ARRAY_BUFFER, quad->vbo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quad->ebo);
     
     glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, quad.tex);
+    glBindTexture(GL_TEXTURE_2D, quad->tex);
 }
 
 void TexturedQuad::DrawQuad()
--- source/graphics/TexturedQuad.h
+++ source/graphics/TexturedQuad.h
@@ -17,26 +17,16 @@ struct TextureQuadObject
 
 namespace TexturedQuad
 {
-    struct TexQuadObj
-    {
-        GLuint vbo, ebo, tex;
-        TexQuadObj();
-    };
-    
     // texSource is the absolute path to the .png source that will be used as the texture.
-    TexQuadObj CreateQuad(std::string texSource);
+    std::shared_ptr<TextureQuadObject> CreateQuad(std::string texSource);
     // texOrig and texSize defines the piece of texSource to be displayed. Values are expected to be in pixel space. 
-    TexQuadObj CreateQuad(std::string texSource, Vector2i texOrig, Vector2i texSize);
+    std::shared_ptr<TextureQuadObject> CreateQuad(std::string texSource, Vector2i texOrig, Vector2i texSize);
     // Update your quad.
-    TexQuadObj UpdateQuad(TexQuadObj quad);
-    // Check if quad is valid.
-    bool IsValidTexQuad(TexQuadObj quad);
+    void UpdateQuad(std::shared_ptr<TextureQuadObject> quad);
 
     // Init before you draw any quads.
     void InitQuadDrawing();
     // Draw and bind your quads as needed.
-    void BindQuad(TexQuadObj quad);
+    void BindQuad(std::shared_ptr<TextureQuadObject> quad);
     void DrawQuad();
-
-    // 
 };
